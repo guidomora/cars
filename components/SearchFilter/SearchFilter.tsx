9b283f3e2ad64841a5cf341d3208a7715241c559
guido morabito <guidomorabito161@hotmail.com>
2023-10-25 18:59:14 -0300

    cars

--- components/SearchFilter/SearchFilter.tsx
+++ components/SearchFilter/SearchFilter.tsx
@@ -1,11 +1,71 @@
-import { Box, Grid } from '@mui/material'
+import { Box, Grid, TextField, Typography, Button, Divider, Select, MenuItem, InputLabel } from '@mui/material';
 import React from 'react'
 
 const SearchFilter = () => {
   return (
     <Grid>
-      <Box bgcolor={"red"} width={250} height={'100vh'}>
-        hola
+      <Box bgcolor={"#f8f9fa"} width={300} height={'100vh'}>
+        <Button variant="contained" color='secondary' sx={{ textTransform: 'none' }}>Buscar</Button>
+        <Divider sx={{ mt: 2 }} />
+        <Box mt={1}>
+          <Typography pl={2} mb={1} fontWeight={500}>Precio</Typography>
+          <Box display={'flex'} justifyContent={'space-around'} alignItems={'center'}>
+            <TextField label='minimo' color='secondary' size='small' sx={{ width: 120 }} /> -
+            <TextField label='maximo' color='secondary' size='small' sx={{ width: 120 }} />
+          </Box>
+        </Box>
+        <Divider sx={{ mt: 3 }} />
+        <Box>
+          <Typography pl={2} mb={1} mt={1} fontWeight={500}>Condición</Typography>
+          <Select sx={{ ml: 2, width: 150 }} color='secondary' size='small'>
+            <MenuItem value="">
+              Ninguna
+            </MenuItem>
+            <MenuItem value={10}>Usado</MenuItem>
+            <MenuItem value={20}>Nuevo</MenuItem>
+          </Select>
+        </Box>
+        <Divider sx={{ mt: 3 }} />
+        <Box>
+          <Typography pl={2} mb={1} mt={1} fontWeight={500}>Marca</Typography>
+          <Select sx={{ ml: 2, width: 150 }} color='secondary' size='small'>
+            <MenuItem value="">
+              Ninguna
+            </MenuItem>
+            <MenuItem value={10}>xxxx</MenuItem>
+            <MenuItem value={20}>xxxx</MenuItem>
+          </Select>
+        </Box>
+        <Divider sx={{ mt: 3 }} />
+        <Box>
+          <Typography pl={2} mb={1} mt={1} fontWeight={500}>Modelo</Typography>
+          <Select sx={{ ml: 2, width: 150 }} color='secondary' size='small'>
+            <MenuItem value="">
+              Ninguna
+            </MenuItem>
+            <MenuItem value={10}>xxxx</MenuItem>
+            <MenuItem value={20}>xxxx</MenuItem>
+          </Select>
+        </Box>
+        <Divider sx={{ mt: 3 }} />
+        <Box mt={1}>
+          <Typography pl={2} mb={1} fontWeight={500}>Kilómetros</Typography>
+          <Box display={'flex'} justifyContent={'space-around'} alignItems={'center'}>
+            <TextField label='minimo' color='secondary' size='small' sx={{ width: 120 }} /> -
+            <TextField label='maximo' color='secondary' size='small' sx={{ width: 120 }} />
+          </Box>
+        </Box>
+        <Divider sx={{ mt: 3 }} />
+        <Box>
+          <Typography pl={2} mb={1} mt={1} fontWeight={500}>Año</Typography>
+          <Select sx={{ ml: 2, width: 150 }} color='secondary' size='small'>
+            <MenuItem value="">
+              Ninguna
+            </MenuItem>
+            <MenuItem value={10}>xxxx</MenuItem>
+            <MenuItem value={20}>xxxx</MenuItem>
+          </Select>
+        </Box>
       </Box>
     </Grid>
   )
--- layoutComponents/header/Header.tsx
+++ layoutComponents/header/Header.tsx
@@ -1,19 +1,30 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
+import { Box, Button, Grid, Link, Typography } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 import logo from "../../img/1.png"
+import NextLink from 'next/link'
 
 const Header = () => {
   return (
     <Grid bgcolor={'black'}>
-      <Box sx={{height:{xs:200, md:100}, flexDirection:{xs:"column", sm:"row"}, justifyContent:{xs:'unset', sm:'space-between'}}} display={'flex'} flexWrap={'wrap'} alignItems={'center'}>
-        <Box sx={{width:{sm:"25vw"}, mt:{xs:3, sm:0}, ml:{xs:0, sm:3}}}>
-          <Image src={logo} alt='logo' height={70} />
+      <Box sx={{ height: { xs: 200, md: 100 }, flexDirection: { xs: "column", sm: "row" }, justifyContent: { xs: 'unset', sm: 'space-between' } }} display={'flex'} flexWrap={'wrap'} alignItems={'center'}>
+        <Box sx={{ width: { sm: "25vw" }, mt: { xs: 3, sm: 0 }, ml: { xs: 0, sm: 3 } }}>
+          <NextLink legacyBehavior href="/" passHref>
+            <Link sx={{ textDecoration: 0 }}>
+              <Image src={logo} alt='logo' height={70} />
+            </Link>
+          </NextLink>
         </Box>
-        <Box sx={{flexDirection:{xs:"column", sm:'row'}, justifyContent:{xs:'space-evenly', sm:'none'}}} display={'flex'} alignItems={'center'}>
-          <Typography sx={{mb:{xs:3, sm:0}}} color={"primary.main"} ml={5} mr={5}>Todo nuestro catálogo</Typography>
+        <Box sx={{ flexDirection: { xs: "column", sm: 'row' }, justifyContent: { xs: 'space-evenly', sm: 'none' } }} display={'flex'} alignItems={'center'}>
+          <Button variant="outlined" sx={{ mb: { xs: 3, sm: 0 }, ml: 5, mr: 5, textTransform: "none" }} >
+            <NextLink legacyBehavior href="/catalogo" passHref>
+              <Link sx={{ textDecoration: 0 }}>
+                Todo nuestro catálogo
+              </Link>
+            </NextLink>
+          </Button>
           <Button variant="outlined" sx={{ color: "secondary.main", ml: 5, mr: 5, textTransform: "none" }}>Contactanos!</Button>
-          <Typography sx={{display:{xs: 'none', sm:'unset'}}} color={"primary.main"} ml={5} mr={5}>loguito</Typography>
+          <Typography sx={{ display: { xs: 'none', sm: 'unset' } }} color={"primary.main"} ml={5} mr={5}>loguito</Typography>
         </Box>
       </Box>
     </Grid>
